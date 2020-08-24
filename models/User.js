fd8569307b38d347c01aceed4e3b4cb4c3d7c412
mr robot <geicarobert@gmail.com>
2020-08-24 16:15:50 +0300

    remove username

--- models/User.js
+++ models/User.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true
-	},
 	email: {
 		type: String,
 		required: true,
