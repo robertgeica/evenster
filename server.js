d8bb67d07687ef2439583c8402607ab28333cc71
mr robot <geicarobert@gmail.com>
2020-08-24 16:31:17 +0300

    auth routes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 
 // routes
-// app.use('/auth', require('./routes/api/auth'));
-// app.use('/register', require('./routes/api/register'));
+app.use('/auth', require('./routes/api/auth'));
+app.use('/register', require('./routes/api/register'));
 // app.use('/pub', require('./routes/api/pub'));
 // app.use('/post', require('./routes/api/pub'));
 
